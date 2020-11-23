0f5d3082a9fe97266602524ec8dc65fc617a67d5
Cyrus Tabatabai-Yazdi <ctabatab@gmail.com>
2020-11-23 00:48:31 -0800

    attempt at adding menu(commented out code)

--- ui.py
+++ ui.py
@@ -7,12 +7,12 @@ from quiz_brain import QuizBrain
 class QuizInterface:
 
     def __init__(self,quiz_brain: QuizBrain):
+
         self.quiz_brain = quiz_brain
         self.window = tkinter.Tk()
         self.window.title("Quizzler")
         self.window.configure(bg=THEME_COLOR,padx=20,pady=20)
-
-        self.score_label = tkinter.Label(text="Score: 0",bg=THEME_COLOR,fg='white')
+        self.score_label = tkinter.Label(text="Score: 0",bg=THEME_COLOR,fg='white',font=('Arial',20,'bold'))
         self.score_label.grid(row=0,column=1,sticky=tkinter.E)
 
         self.canvas = tkinter.Canvas(width=300,height=250,bg='white')
@@ -38,8 +38,54 @@ class QuizInterface:
         self.get_next_question()
 
         self.window.mainloop()
-    
+    ''' 
+    def _play_game(self):
+        self.score_label = tkinter.Label(text="Score: 0",bg=THEME_COLOR,fg='white',font=('Arial',20,'bold'))
+        self.score_label.grid(row=0,column=1,sticky=tkinter.E)
 
+        self.canvas = tkinter.Canvas(width=300,height=250,bg='white')
+        self.text = self.canvas.create_text(150,125,text="",font=('Arial',20,'italic'),width=280)
+        self.canvas.grid(row=1,column=0,columnspan=2,pady=50)
+
+        
+        self.true_image = tkinter.PhotoImage(file='images/true.png')
+        self.false_image = tkinter.PhotoImage(file='images/false.png')
+
+        self.true_button = tkinter.Button(image=self.true_image,command=self._clicked_true)
+        self.true_button.grid(row=2,column=0)
+
+        self.false_button = tkinter.Button(image=self.false_image,command=self._clicked_false)
+        self.false_button.configure(pady=10)
+        self.false_button.grid(row=2,column=1)
+
+        self.reset_button = tkinter.Button(text="PLAY AGAIN!",font=("Arial",40,'normal'),command=self.reset)
+
+        self.reset_button.grid(row=3,column=0,columnspan=2,pady=50)
+        self.reset_button.grid_remove()
+
+        self.get_next_question()
+
+
+    def _menu(self):
+
+        title_label = tkinter.Label(text="QUIZZLER",bg=THEME_COLOR,fg='black',font=('Arial',20,'bold'))
+        title_label.pack()
+        
+        def start_game():
+            title_label.pack_forget()
+            play_button.pack_forget()
+            self._play_game()
+            
+
+        play_button = tkinter.Button(text="PLAY",command=start_game)
+        play_button.pack()
+
+        self.window.mainloop()
+
+    '''
+
+
+    
     def reset(self):
         self.quiz_brain.reset()
         self.reset_button.grid_remove()
